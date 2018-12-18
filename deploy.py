c58123e91c9aaafc292d5d1d41864875f0f9d9cc
Anusha Swamy <31597958+AnushaS95@users.noreply.github.com>
2018-12-18 19:22:39 +0530

    Api to Json

--- deploy.py
+++ deploy.py
@@ -3,6 +3,8 @@
 from flask import Flask
 import sqlite3
 from flask import request
+import json
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -14,8 +16,10 @@ def retrieve():
    statement = "SELECT * from users where id = " + user
    iterator.execute(statement)
    results = iterator.fetchall()
-   display = "4 Most similar users to User {0} are <br><br>User - {1} with similarity of {2}<br>User - {3} with similarity of {4}<br>User - {5} with similarity of {6}<br>User - {7} with similarity of {8}".format(user, results[0][1], results[0][2], results[0][3], results[0][4], results[0][5], results[0][6], results[0][7], results[0][8])
-   return display
+   data = {"user": user, "similar_users": [{"user": results[0][1], "similarity":results[0][2]}, {"user":results[0][3], "similarity":results[0][4]}, {"user":results[0][5], "similarity":results[0][6]}, {"user":results[0][7], "similarity":results[0][8]}]}
+   data = jsonify(data)
+   print data.get_json()['user'], data.get_json()['similar_users'][3]['user'], data.get_json()['similar_users'][3]['similarity']
+   return data
 
 if __name__ == "__main__":
    app.run()

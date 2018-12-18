8ee5de003a0aec5d5037eadbaf6d7fd86452d98f
Anusha Swamy <31597958+AnushaS95@users.noreply.github.com>
2018-12-18 17:46:43 +0530

    Added db creator and the db itself

--- main.py
+++ main.py
@@ -32,3 +32,23 @@ result =  pd.concat([interests, authors, courses], axis=1)
 result = result.fillna(0)
 
 print result.head()
+
+similarity_matrix = pd.DataFrame(cosine_similarity(result))
+print similarity_matrix.head(2)
+
+db = sqlite3.connect("similarity.db")
+iterator = db.cursor()
+iterator.execute("create table users (id, sim1_user, sim1_similarity, sim2_user, sim2_similarity, sim3_user, sim3_similarity, sim4_user, sim4_similarity)")
+
+for index, row in similarity_matrix.iterrows():
+   a = np.array(row)
+   user_ids = a.argsort()[-5:][::-1]
+   similarities = np.take(a, user_ids)
+   user_ids = user_ids + 1
+   print user_ids
+   print similarities
+   statement = "insert into users values ({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8})".format(index+1, user_ids[1], similarities[1], user_ids[2], similarities[2], user_ids[3], similarities[3], user_ids[4], similarities[4])
+   print statement
+   iterator.execute(statement)
+
+db.commit()

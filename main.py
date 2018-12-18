6a58788027142aff2a36cc2a20d810f9cc4bb664
Anusha Swamy <31597958+AnushaS95@users.noreply.github.com>
2018-12-18 17:15:23 +0530

    Added user author matrix and user course matrix

--- main.py
+++ main.py
@@ -9,7 +9,26 @@ def user_interest_matrix():
     df = df.groupby(['user_handle', 'interest_tag']).size().unstack(fill_value=0).reset_index()
     return df
 
+def user_author_matrix():
+    df = pd.read_csv('user_course_views.csv')
+    df = df.drop_duplicates(subset=['user_handle','author_handle'], keep="first")
+    df = df.groupby(['user_handle','author_handle']).size().unstack(fill_value=0).reset_index()
+    return df
+
+def user_course_matrix():
+    df = pd.read_csv("user_course_views.csv")
+    df = df.drop_duplicates(subset=['user_handle','course_id'],keep="first")
+    df = df.groupby(['user_handle','course_id']).size().unstack(fill_value=0).reset_index()
+    return df   
+
 interests = user_interest_matrix()
 interests = interests.set_index('user_handle')
+authors = user_author_matrix()
+authors = authors.set_index('user_handle')
+courses = user_course_matrix()
+courses = courses.set_index('user_handle')
 
-print interests
+result =  pd.concat([interests, authors, courses], axis=1)
+result = result.fillna(0)
+
+print result.head()
